445f2354e85b77d8f7cfa0c95d31434b66969bce
ParrishJ <jarparrish@gmail.com>
2019-06-04 12:58:11 -0700

    got route set up, imported knex

---
+++ zoo/zoo-router.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+
+const router = require('express').Router();
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/rolex.db3'
+    },
+    useNullAsDefault: true,
+};
+
+const db = knex(knexConfig);
+
+module.exports = router;
