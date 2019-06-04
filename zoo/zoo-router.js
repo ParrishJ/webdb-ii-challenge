189210bbaccf3b2c913cbdf5780c72d287b00b1e
ParrishJ <jarparrish@gmail.com>
2019-06-04 14:23:32 -0700

    all crud opps added

--- zoo/zoo-router.js
+++ zoo/zoo-router.js
@@ -49,5 +49,37 @@ router.get('/:id', (req, res) => {
         })
 });
 
+router.put('/:id', (req, res) => {
+    const changes = req.body
+    db('zoos')
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ message: 'Zoo not fount' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    db('zoos')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count > 0) {
+                const unit = count > 1 ? 'zoos' : 'zoo'
+                res.status(200).json({ message: `${count} ${unit} deleted` })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
 
 module.exports = router;
